68184850bcdfee137959cb6d5f0f8d94c2ac95be
JianTuanGao <JianTuanGao@DESKTOP-UI15NSI>
2019-12-15 18:02:32 +0800

    first commit

---
+++ stock-zuul/src/test/java/com/gao/demo/StockZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
